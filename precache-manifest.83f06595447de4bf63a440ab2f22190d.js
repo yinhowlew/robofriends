8709720c56ae771216ef590cc727dc1c57e88750
yinhow <yinhowlew@gmail.com>
2019-12-28 11:45:37 -0500

    Updates

---
+++ precache-manifest.83f06595447de4bf63a440ab2f22190d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bd68680197ccab979e067c417f7b082",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "d970f485385d687bf64b",
+    "url": "/robofriends/static/css/main.c90f86f2.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "d970f485385d687bf64b",
+    "url": "/robofriends/static/js/main.52a80051.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
